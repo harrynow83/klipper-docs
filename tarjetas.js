e4af9aede4d3f9dd946a1e4050b510e43f1c10a5
harrynow83 <morenomaldito@gmail.com>
2026-01-10 01:06:11 +0100

    deploy 2026-01-10 01:06

--- tarjetas.js
+++ tarjetas.js
@@ -2,39 +2,47 @@ const cardsData = [
     {
         title: "Tarjeta 1",
         description: "Descripción de la primera tarjeta.",
-        image: "https://picsum.photos/400/300?random=1"
+        image: "img/kiauh.png",
+        link: "detalle1.html"
     },
     {
         title: "Tarjeta 2",
         description: "Descripción de la segunda tarjeta.",
-        image: "https://picsum.photos/400/300?random=2"
+        image: "img/kiauh.png",
+        link: "detalle2.html"
     },
     {
         title: "Tarjeta 3",
         description: "Descripción de la tercera tarjeta.",
-        image: "https://picsum.photos/400/300?random=3"
+        image: "img/mellow-compile.png",
+        link: "https://www.google.com"
     },
     {
         title: "Tarjeta 4",
         description: "Descripción de la cuarta tarjeta.",
-        image: "https://picsum.photos/400/300?random=4"
+        image: "img/pi-zero.jpg",
+        link: "#"
     }
 ];
 
 const container = document.getElementById("cardContainer");
 
 cardsData.forEach(card => {
+    const link = document.createElement("a");
+    link.href = card.link;
+    link.classList.add("card-link");
+
     const cardElement = document.createElement("div");
     cardElement.classList.add("card");
 
     cardElement.innerHTML = `
-        <img src="${card.image}" alt="${card.title}">
+        <img src="${card.image}" alt="${card.title}" loading="lazy">
         <div class="card-content">
             <h3>${card.title}</h3>
             <p>${card.description}</p>
         </div>
     `;
 
-    container.appendChild(cardElement);
+    link.appendChild(cardElement);
+    container.appendChild(link);
 });
-
